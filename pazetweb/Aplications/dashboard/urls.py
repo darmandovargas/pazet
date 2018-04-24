d4fc16c8f07d5f6c75e5b89184031d59e7e36fa0
Diego Vargas <diego@thinkcloudgroup.com>
2018-04-24 10:48:26 -0500

    2.2 Poligono de Thiessen
    2.2 Crear archivos shape de sub-areas de influencia dependiendo de la ubicación de los puntos de suelos. (Método Poligonos de Thessen).
    Se ha habilitado la compatibilidad con el lector de pantalla.
     
     
     				
    
    Crear archivos shape de sub-areas de influencia dependiendo de la ubicación de los puntos de suelos. (Método Poligonos de Thessen).
    Se creó la fuperficie del polígono de Thiessen en el mapa por medio del link de suelos, ya se tiene parte del script en python para cuando haya que hacer el análisis de los puntos para crear los archivos planos.

--- pazetweb/Aplications/dashboard/urls.py
+++ pazetweb/Aplications/dashboard/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
 
 	url(r'^index$', views.index, name='index'),
 	url(r'^mapainteractivo', views.mapainteractivo, name='mapainteractivo'),
+	url(r'^mapainteractivo/voronoi', views.mapainteractivo, name='mapainteractivo'),
+	url(r'^point.geojson', views.getgeojson, name='getgeojson'),
 
 	#url(r'^mapainteractivo/$', views.mapainteractivo, name='principal.mapainteractivo'),
 ]
